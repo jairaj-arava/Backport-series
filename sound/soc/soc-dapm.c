b7132737b8f2242ff2d45932334d99013962d3ce
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2021-08-05 10:11:24 +0900

    UPSTREAM: ASoC: soc-dapm: cleanup cppcheck warning at dapm_seq_run()
    This patch cleanups below cppcheck warning.
    
    sound/soc/soc-dapm.c:1648:6: style: The scope of the variable 'ret' can be reduced. [variableScope]
     int ret, i;
         ^
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Link: https://lore.kernel.org/r/87pmusu24j.wl-kuninori.morimoto.gx@renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit a71657947d74fbc9e0184079f9e1941d70ccb52c)
    
    BUG=b:238403794
    TEST=Test Audio use cases.
    
    Signed-off-by: sboyane <shankarx.boyane@intel.com>
    Change-Id: I40dcf7c53bd45ac8a76b20c2fd13cd842a58829c

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -1649,7 +1649,7 @@ static void dapm_seq_run(struct snd_soc_card *card,
 	int cur_subseq = -1;
 	int cur_reg = SND_SOC_NOPM;
 	struct snd_soc_dapm_context *cur_dapm = NULL;
-	int ret, i;
+	int i;
 	int *sort;
 
 	if (power_up)
@@ -1658,7 +1658,7 @@ static void dapm_seq_run(struct snd_soc_card *card,
 		sort = dapm_down_seq;
 
 	list_for_each_entry_safe(w, n, list, power_list) {
-		ret = 0;
+		int ret = 0;
 
 		/* Do we need to apply any queued changes? */
 		if (sort[w->id] != cur_sort || w->reg != cur_reg ||
