525228ef4d8de47b45124305869295a1bbe2e7d2
Brent Lu <brent.lu@intel.com>
2021-09-14 18:18:45 +0800

    BACKPORT: ASoC: intel: sof_rt5682: support jsl_rt5682s_rt1015p board
    This patch adds driver data for two ALC1015Q-VB speaker amplifiers on
    SSP1 and one ALC5682I-VS headphone codec on SSP0 for JSL platform.
    
    Topology is leveraged from jsl_rt5682_rt1015p since the capability of
    two ALC5682 variants is the same.
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20210914101847.778688-3-brent.lu@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 46414bc325df42ed0b18a50e2ee707e0424163a8)
    
     Conflicts:
    	sound/soc/intel/boards/sof_rt5682.c
    (mdolke) added support jsl_rt5682s_rt1015p board
    
    BUG=b:218766331
    TEST=Test sound cards are listed and audio playback works.
    
    Signed-off-by: Terry Cheong <htcheong@chromium.org>
    Signed-off-by: Madhulika Dolke <madhulika.dolke@intel.com>
    (cherry picked from commit 46414bc325df42ed0b18a50e2ee707e0424163a8)
    Change-Id: I2e3a7c6f172c21f478ab470911928ec5d1e01b48

--- sound/soc/intel/boards/sof_rt5682.c
+++ sound/soc/intel/boards/sof_rt5682.c
@@ -966,6 +966,16 @@ static const struct platform_device_id board_ids[] = {
 					SOF_BT_OFFLOAD_SSP(2) |
 					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
+	{
+		.name = "jsl_rt5682s_rt1015p",
+		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
+					SOF_RT5682_MCLK_24MHZ |
+					SOF_RT5682_SSP_CODEC(0) |
+					SOF_RT5682S_HEADPHONE_CODEC_PRESENT |
+					SOF_SPEAKER_AMP_PRESENT |
+					SOF_RT1015P_SPEAKER_AMP_PRESENT |
+					SOF_RT5682_SSP_AMP(1)),
+	},
 	{ }
 };
 MODULE_DEVICE_TABLE(platform, board_ids);
