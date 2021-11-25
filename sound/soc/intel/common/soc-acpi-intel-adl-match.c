e7727aa69ec3aeb9436ea89891379e32fcde816c
Mac Chiang <mac.chiang@intel.com>
2021-11-24 22:04:53 -0500

    BACKPORT: ASoC: Intel: boards: add max98390 2/4 speakers support
    support 2 hw boards.
    1. SSP2 connects max98390, 2 speakers.
    2. SSP1 connects max98390, 2/4 speakers.
    
    2 or 4 speakers playback
    add echo reference capture
    add bt offload support
    add DMI_OEM_STRING for board variants
    add ALC5682I-VS support
    
    Signed-off-by: Mark Hsieh <mark_hsieh@wistron.corp-partner.google.com>
    Signed-off-by: Mac Chiang <mac.chiang@intel.com>
    Signed-off-by: Kieth Tzeng <keith.tzeng@quantatw.com>
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20211125030453.4382-1-mac.chiang@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit f316c9d9ba8ea08d6994bc5ba8fa276eab186208)
    
     Conflicts:
    	sound/soc/intel/common/soc-acpi-intel-adl-match.c
    	sound/soc/sof/sof-pci-dev.c
    
    BUG=b:218766331
    TEST=Test sound cards are listed and audio playback works.
    
    Signed-off-by: Madhulika Dolke <madhulika.dolke@intel.com>
    Change-Id: I3a31b7200547bf0eb96da50fa824c73291aecb9f

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -379,6 +379,11 @@ static const struct snd_soc_acpi_codecs adl_rt5682_rt5682s_hp = {
 	.codecs = {"10EC5682", "RTL5682"},
 };
 
+static const struct snd_soc_acpi_codecs adl_max98390_amp = {
+	.num_codecs = 1,
+	.codecs = {"MX98390"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 	{
 		.comp_ids = &adl_rt5682_rt5682s_hp,
@@ -426,6 +431,14 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.id = "10508825",
 		.drv_name = "sof_nau8825",
 		.sof_tplg_filename = "sof-adl-nau8825.tplg",
+	},
+	{
+		.comp_ids = &adl_rt5682_rt5682s_hp,
+		.drv_name = "adl_max98390_rt5682",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &adl_max98390_amp,
+		.sof_fw_filename = "sof-adl.ri",
+		.sof_tplg_filename = "sof-adl-max98390-rt5682.tplg",
 	},
 	{
 		.comp_ids = &adl_rt5682_rt5682s_hp,
